3e401b4a269d221cf8920b4210106f8589012f54
Pooja <pooja@Poojas-MacBook-Air.local>
2026-04-20 22:18:14 +0530

    main class change

--- src/main/java/springStarter/SpringStarterApplication.java
+++ src/main/java/springStarter/SpringStarterApplication.java
@@ -2,11 +2,18 @@ package springStarter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-public class SpringStarterApplication {
+public class SpringStarterApplication extends SpringBootServletInitializer{
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringStarterApplication.class);
+    }
 	
 	
 	public static void main(String[] args)
