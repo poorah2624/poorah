f6d7c3f851158a3ab974c0f8b1bec1c95fa6cf95
Pooja <pooja@Poojas-MacBook-Air.local>
2026-05-15 23:28:00 +0530

    page fix

--- src/main/java/springStarter/models/ItemVariant.java
+++ src/main/java/springStarter/models/ItemVariant.java
@@ -2,12 +2,8 @@ package springStarter.models;
 
 import javax.persistence.*;
 
-import lombok.*;
-
 @Entity
 @Table(name = "item_variant")
-@Getter
-@Setter
 public class ItemVariant {
 
     @Id
@@ -27,6 +23,64 @@ public class ItemVariant {
     @ManyToOne
     @JoinColumn(name = "item_id")
     private Item item;
+
+	public Long getVariantId() {
+		return variantId;
+	}
+
+	public void setVariantId(Long variantId) {
+		this.variantId = variantId;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	public Integer getStock() {
+		return stock;
+	}
+
+	public void setStock(Integer stock) {
+		this.stock = stock;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getSku() {
+		return sku;
+	}
+
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+
+	public Item getItem() {
+		return item;
+	}
+
+	public void setItem(Item item) {
+		this.item = item;
+	}
+    
+    
 
     
 }
