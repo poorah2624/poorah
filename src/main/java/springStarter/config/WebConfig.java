ce46ccd608181ec2ac764e32e95229672546ae27
Pooja <pooja@Poojas-MacBook-Air.local>
2026-06-02 22:57:03 +0530

    changes

--- src/main/java/springStarter/config/WebConfig.java
+++ src/main/java/springStarter/config/WebConfig.java
@@ -7,19 +7,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    /*@Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:"+ System.getProperty("user.dir") + "/src/main/resources/static/uploads/");
-    }*/
-    
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    	
-    	registry.addResourceHandler("/**")
-        .addResourceLocations("classpath:/static/");
         
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:/app/uploads/");
+                .addResourceLocations("file:/opt/uploads/");
+                
+       
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
     }
 }
